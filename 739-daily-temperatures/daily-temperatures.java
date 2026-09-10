de0eeb9c39d8d3e3ee0589a16f57f1d663ce723e
Vineet Prakash <vineetprakash918@gmail.com>
2026-09-10 16:58:33 +0530

    Time: 23 ms (92.94%) | Memory: 106.9 MB (56.22%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int[] answer = new int[temperatures.length];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() &&
+                   temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                answer[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
